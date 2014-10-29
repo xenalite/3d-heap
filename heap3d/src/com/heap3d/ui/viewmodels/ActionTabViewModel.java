4280a2b6502bdff30ca0517366058a8843106a09
Oskar Mamrzynski <oskar.mamrzynski12@imperial.ac.uk>
2014-10-29 21:02:09 +0000

    Added basic UI from other repository.

---
+++ heap3d/src/com/heap3d/ui/viewmodels/ActionTabViewModel.java
@@ -0,0 +1,30 @@
+package com.heap3d.ui.viewmodels;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by oskar on 29/10/14.
+ */
+public class ActionTabViewModel {
+    private StringProperty _newBreakpoint;
+
+    public void pauseAction() {
+
+    }
+
+    public void resumeAction() {
+
+    }
+
+    public void stepAction() {
+
+    }
+
+    public void addAction() {
+
+    }
+
+    public StringProperty getNewBreakpointProperty() {
+        return _newBreakpoint;
+    }
+}
---
+++ heap3d/src/com/heap3d/ui/viewmodels/MainWindowViewModel.java
@@ -0,0 +1,7 @@
+package com.heap3d.ui.viewmodels;
+
+/**
+ * Created by oskar on 29/10/14.
+ */
+public class MainWindowViewModel {
+}
