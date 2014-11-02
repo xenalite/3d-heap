3600d2932a93b45eef34f913770693549b93a32f
Niklas Steidl <nms12@ic.ac.uk>
2014-11-02 11:48:54 +0000

    Unit tests added for springBasedLayout

--- Layout/src/com/heap3d/layout/SpringBasedLayout.java
+++ Layout/src/com/heap3d/layout/SpringBasedLayout.java
@@ -1,18 +1,14 @@
 package com.heap3d.layout;
 
 import com.heap3d.Node;
-import org.gephi.graph.api.*;
-import org.gephi.graph.dhns.core.GraphFactoryImpl;
-import org.gephi.graph.dhns.node.AbstractNode;
-import org.gephi.io.generator.plugin.RandomGraph;
-import org.gephi.io.importer.api.ContainerFactory;
-import org.gephi.io.importer.api.ImportController;
-import org.gephi.io.processor.plugin.DefaultProcessor;
-import org.gephi.layout.plugin.force.StepDisplacement;
-import org.gephi.layout.plugin.force.yifanHu.YifanHuLayout;
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphFactory;
+import org.gephi.graph.api.GraphModel;
 import org.gephi.layout.plugin.fruchterman.FruchtermanReingold;
-import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
+import org.gephi.project.impl.ProjectImpl;
+import org.gephi.workspace.impl.WorkspaceImpl;
 import org.openide.util.Lookup;
 
 import java.util.HashMap;
@@ -22,7 +18,8 @@ public class SpringBasedLayout implements LayoutService {
     @Override
     public Map<String, LayoutNode> layout(Map<String, Node> nodes) {
 
-        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel();
+        Workspace ws = new WorkspaceImpl(new ProjectImpl());
+        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel(ws);
         DirectedGraph graph = graphModel.getDirectedGraph();
 
         //Add all the nodes
@@ -50,7 +47,7 @@ public class SpringBasedLayout implements LayoutService {
 //        layout.setOptimalDistance(20f);
 
         for (int i = 0; i < 100 && layout.canAlgo(); i++) {
-            System.out.println("Laying Out ..");
+//            System.out.println("Laying Out ..");
             layout.goAlgo();
         }
         layout.endAlgo();
@@ -58,12 +55,14 @@ public class SpringBasedLayout implements LayoutService {
         Map<String, LayoutNode> map = new HashMap<String, LayoutNode>();
         for(org.gephi.graph.api.Node n : graph.getNodes())
         {
-            map.put(n.getNodeData().getId(), new LayoutNode(
+            LayoutNode newNode = new LayoutNode(
                     n.getNodeData().getId(),
                     n.getNodeData().x(),
                     n.getNodeData().y(),
                     n.getNodeData().z()
-            ));
+            );
+            newNode.getChildren().addAll(nodes.get(newNode.getId()).getChildren());
+            map.put(n.getNodeData().getId(), newNode);
         }
 
         return map;
---
+++ Layout/test/com/heap3d/layout/SpringBasedLayoutTest.java
@@ -0,0 +1,111 @@
+package com.heap3d.layout;
+
+import com.heap3d.Node;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphModel;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openide.util.Lookup;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class SpringBasedLayoutTest {
+
+    private static Boolean areNodesEqual(Node originalNode, Node newNode) {
+        Boolean equal = true;
+        equal &= originalNode.getId().equals(newNode.getId());
+
+        List<Node> oChildren = originalNode.getChildren();
+        List<Node> nChildren = newNode.getChildren();
+        if (oChildren.size() == nChildren.size()) {
+            for (int i = 0; i < originalNode.getChildren().size(); i++) {
+                equal &= oChildren.get(i).getId().equals(nChildren.get(i).getId());
+            }
+        } else
+        {
+            return false;
+        }
+
+        return equal;
+    }
+
+    @Test
+    public void testLayoutEmptyPass() throws Exception {
+
+        SpringBasedLayout sl = new SpringBasedLayout();
+
+        Map<String, Node> graph = new HashMap<String, Node>();
+        Map<String, LayoutNode> layout = sl.layout(graph);
+
+        assertEquals(layout.size(), graph.size());
+    }
+
+    @Test
+    public void testLayoutSinglePass() throws Exception {
+
+        SpringBasedLayout sl = new SpringBasedLayout();
+
+        Map<String, Node> graph = new HashMap<String, Node>();
+        Node n = new LayoutNode("id", 0, 0, 0);
+        graph.put(n.getId(), n);
+        Map<String, LayoutNode> layout = sl.layout(graph);
+
+        assertTrue(layout.containsKey(n.getId()));
+        Node layoutNode = layout.get(n.getId());
+        assertTrue(areNodesEqual(n, layoutNode));
+    }
+
+    @Test
+    public void testLayoutPairNonDestructivePass() throws Exception {
+
+        SpringBasedLayout sl = new SpringBasedLayout();
+
+        Map<String, Node> graph = new HashMap<String, Node>();
+        Node a = new LayoutNode("a", 0, 0, 0);
+        Node b = new LayoutNode("b", 0, 0, 0);
+        a.getChildren().add(b);
+        b.getChildren().add(a);
+        graph.put(a.getId(), a);
+        graph.put(b.getId(), b);
+        Map<String, LayoutNode> layout = sl.layout(graph);
+
+        for (Node n : graph.values()) {
+            assertTrue(layout.containsKey(n.getId()));
+            Node layoutNode = layout.get(n.getId());
+            assertTrue(areNodesEqual(n, layoutNode));
+        }
+    }
+
+    @Test
+    public void testLayoutPairNonDestructiveFail() throws Exception {
+
+        SpringBasedLayout sl = new SpringBasedLayout();
+
+        Map<String, Node> graph = new HashMap<String, Node>();
+        Node a = new LayoutNode("a", 0, 0, 0);
+        Node b = new LayoutNode("b", 0, 0, 0);
+        a.getChildren().add(b);
+        b.getChildren().add(a);
+        graph.put(a.getId(), a);
+        graph.put(b.getId(), b);
+        Map<String, LayoutNode> layout = sl.layout(graph);
+
+        for (Node n : graph.values()) {
+            assertTrue(layout.containsKey(n.getId()));
+            Node layoutNode = layout.get(n.getId());
+            //Remove the children
+            layoutNode.getChildren().clear();
+            //this should fail
+            assertFalse(areNodesEqual(n, layoutNode));
+        }
+    }
+
+}
