73752738492af26302930e57bd40da8065a56800
Stephen James <stephen.james12@imperial.ac.uk>
2014-11-13 20:28:43 +0000

    Improved perf test with addition of monitoring performance of lines being drawn. Plus, minor changes to graphics lib: ability to clear all objects

--- Tests/GraphicsPerf/src/com/perf/PerfTest.java
+++ Tests/GraphicsPerf/src/com/perf/PerfTest.java
@@ -12,7 +12,8 @@ public class PerfTest extends RenderEngine{
 
 	private Random rand;
 	private Colour c;
-	private StringBuilder stringBuilder;
+	private Cube centre;
+	private StringBuilder cubesStringBuilder;
 	private static final int MAX_CUBES = 15000;
 	
 	public static void main(String[] args){
@@ -29,39 +30,59 @@ public class PerfTest extends RenderEngine{
 	protected void beforeLoop() {
 		rand = new Random();
 		c = new Colour(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
-		stringBuilder = new StringBuilder();
+		cubesStringBuilder = new StringBuilder();
+		System.out.print("Starting cubes perf test");
 	}
 
+	private boolean finishedCubeTest, finishedLineTest;
 	
 	@Override
 	protected void inLoop() {
-		
-		int entities = getNumberOfEntities();
-		
-		if(entities > MAX_CUBES)
-			breakOutOfLoop();
-		
+	
 		float x = rand.nextFloat() * 100 -50;
 		float y = rand.nextFloat() * 100 -50;
 		float z = rand.nextFloat() * -300;
+
+		if(!finishedCubeTest){
+			
+			cubeTest(x, y, z);
+			
+			if(getNumberOfEntities() >= MAX_CUBES){
+				finishedCubeTest = true;
+				clearEntitiesFrom3DSpace();
+				writeResults("cubes");
+				cubesStringBuilder = new StringBuilder();
+				System.out.print("Starting lines perf test");
+				centre = new Cube(0, 0, -150, rand.nextFloat()*180f, rand.nextFloat()*180f, 0f, 1f, c);
+				addEntityTo3DSpace(centre);
+			}
+			
+		}else if(!finishedLineTest){
+
+			lineTest(x, y, z);
+			
+			if(getNumberOfEntities() >= MAX_CUBES){
+				finishedLineTest = true;
+				writeResults("lines");
+				
+			}
+			
+		}else{
+			breakOutOfLoop();
+		}
 		
-		addEntityTo3DSpace(new Cube(x, y, z, rand.nextFloat()*180f, rand.nextFloat()*180f, 0f, 1f, c));
-		
-		String output = entities + " " + getFPS() + "\n";
-		
-		if(entities % 50 == 0)
-			System.out.print(output);
-		
-		stringBuilder.append(output);
 	}
 
 	@Override
 	protected void afterLoop() {
+
+	}
+	
+	private void writeResults(String filename){
 		PrintWriter writer = null;
 		try {
-			writer = new PrintWriter("data.data", "UTF-8");
-			writer.print(stringBuilder.toString());
-			writer.close();
+			writer = new PrintWriter(filename+".data", "UTF-8");
+			writer.print(cubesStringBuilder.toString());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
@@ -70,6 +91,33 @@ public class PerfTest extends RenderEngine{
 			if(writer != null)
 				writer.close();
 		}
+	}
+	
+	private void cubeTest(float x, float y, float z){
+		
+		addEntityTo3DSpace(new Cube(x, y, z, rand.nextFloat()*180f, rand.nextFloat()*180f, 0f, 1f, c));
+		
+		String output = getNumberOfEntities() + " " + getFPS() + "\n";
+		
+		if(getNumberOfEntities() % 50 == 0)
+			System.out.print(output);
+		
+		cubesStringBuilder.append(output);
+	}
+	
+	private void lineTest(float x, float y, float z){
+		
+		Cube cube = new Cube(x, y, z, rand.nextFloat()*180f, rand.nextFloat()*180f, 0f, 1f, c);
+		addEntityTo3DSpace(cube);
+		
+		centre.addConnection(cube, null);
+		
+		String output = getNumberOfEntities() + " " + getFPS() + "\n";
+		
+		if(getNumberOfEntities() % 50 == 0)
+			System.out.print(output);
+		
+		cubesStringBuilder.append(output);
 	}
 	
 }
