1d3042711e1544c9c082f7e2745efedf0b63f4c4
Niklas Steidl <nms12@ic.ac.uk>
2014-11-18 14:44:37 +0000

    new Heapviz implementation started

--- HeapVisualizer/src/com/heap3d/Main.java
+++ HeapVisualizer/src/com/heap3d/Main.java
@@ -1,15 +1,8 @@
 package com.heap3d;
 
-
-import com.heap3d.layout.CircularLayout;
-import com.heap3d.layout.FRLayout;
-import com.heap3d.layout.ISOLayout;
-import com.heap3d.layout.IterativeSpringBasedLayout;
-
 public class Main  {
 
-    public static void main(String[] args)
-    {
-        HeapGraph h = new HeapGraph(new IterativeSpringBasedLayout());
+    public static void main(String[] args) throws InterruptedException {
+        HeapGraph h = new HeapGraph();
     }
 }
