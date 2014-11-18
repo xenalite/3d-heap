349507892c9c986c50ede4bd7cac22ff36ebef68
Niklas Steidl <nms12@ic.ac.uk>
2014-11-18 18:50:24 +0000

    Other layouts updated, and tested

--- Layout/src/com/heap3d/layout/CircularLayout.java
+++ Layout/src/com/heap3d/layout/CircularLayout.java
@@ -1,12 +1,23 @@
 package com.heap3d.layout;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
-import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.*;
 
 import java.awt.*;
 
-//public class CircularLayout extends JungLayout{
-//
+public class CircularLayout <V,E> extends JungLayout<V,E>{
+
+    CircleLayout<V,E> sl;
+
+    public CircularLayout(edu.uci.ics.jung.graph.Graph<V,E> graph)
+    {
+        sl = new CircleLayout<V,E>(graph);
+        this.layout = sl;
+        setSize(new Dimension(1000,1000));
+        //sl.setRadius(500);
+
+    }
+
 //    @Override
 //    protected void initializeLayout(DirectedGraph g, Dimension size) {
 //        CircleLayout sl = new CircleLayout(g);
@@ -15,11 +26,11 @@ import java.awt.*;
 //        sl.setRadius(+size.getHeight()/2.01f);
 //        sl.initialize();
 //    }
-//
-//    @Override
-//    protected void run() {
-//        //Nothing to do, layout is run in a single step
-//    }
-//}
+
+    @Override
+    protected void run() {
+        //Nothing to do, layout is run in a single step
+    }
+}
 
 
--- Layout/src/com/heap3d/layout/ISOLayout.java
+++ Layout/src/com/heap3d/layout/ISOLayout.java
@@ -1,29 +1,31 @@
 package com.heap3d.layout;
 
 import edu.uci.ics.jung.algorithms.layout.ISOMLayout;
-import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.*;
 
 import java.awt.*;
 
 
-//public class ISOLayout extends JungLayout{
-//
-//    ISOMLayout sl;
-//    @Override
-//    protected void initializeLayout(DirectedGraph g, Dimension size) {
-//        sl = new ISOMLayout(g);
-//        layout = sl;
-//        sl.setSize(size);
-//        sl.initialize();
-//    }
-//
-//    @Override
-//    protected void run() {
-//        int i=0;
-//        while (i<100 && !sl.done())
-//        {
-//            sl.step();
-//            i++;
-//        }
-//    }
-//}
+public class ISOLayout<V,E> extends JungLayout<V,E>{
+
+    ISOMLayout sl;
+
+    public ISOLayout(edu.uci.ics.jung.graph.Graph<V,E> graph)
+    {
+        sl = new ISOMLayout<V,E>(graph);
+        this.layout = sl;
+        setSize(new Dimension(1000,1000));
+    }
+
+    @Override
+    protected void run() {
+        int i=0;
+        while (i<100 && !sl.done())
+        {
+            sl.step();
+            i++;
+        }
+    }
+
+
+}
