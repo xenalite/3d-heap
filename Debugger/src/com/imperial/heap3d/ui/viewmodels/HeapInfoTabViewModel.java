065c38c45dacab2bbdcfdee7553eb3877bc39147
Niklas Steidl <nms12@ic.ac.uk>
2014-11-29 11:56:36 +0000

    TreeView nodes changed to lazyLoad children to avoid stack overflow

--- Debugger/src/com/imperial/heap3d/ui/viewmodels/HeapInfoTabViewModel.java
+++ Debugger/src/com/imperial/heap3d/ui/viewmodels/HeapInfoTabViewModel.java
@@ -32,19 +32,20 @@ public class HeapInfoTabViewModel {
     private StringProperty _HeapInfo;
 
     private SimpleObjectProperty<TreeItem<Node>> _TreeView;
-    public  SimpleObjectProperty<TreeItem<Node>> TreeViewProperty() {
+
+    public SimpleObjectProperty<TreeItem<Node>> TreeViewProperty() {
         return _TreeView;
     }
 
 
     public HeapInfoTabViewModel(EventBus eventBus) {
-        if(eventBus == null)
+        if (eventBus == null)
             throw new IllegalArgumentException("eventBus");
         _eventBus = eventBus;
         _eventBus.register(this);
 
         _HeapInfo = new SimpleStringProperty(this, "", "Data from the ViewModel");
-        _TreeView = new SimpleObjectProperty<>(this,"", new TreeItem<>());
+        _TreeView = new SimpleObjectProperty<>(this, "", new TreeItem<>());
     }
 
 
@@ -53,44 +54,84 @@ public class HeapInfoTabViewModel {
         try {
             switch (pe.type) {
                 case SELECT:
-                    Platform.runLater( () -> {
+                    Platform.runLater(() -> {
                         _HeapInfo.set(pe.message);
-                    })  ;
+                    });
 
                     break;
 
             }
+        } catch (IllegalStateException e) {
+            System.out.println(e);
         }
-        catch(IllegalStateException e) { System.out.println(e); }
     }
 
 
     @Subscribe
     public void handleNodeEvent(NodeEvent ne) {
-        Platform.runLater( () -> {
+        Platform.runLater(() -> {
             TreeItem<Node> root = new TreeItem<>(null);
             root.setExpanded(true);
             ObservableList<TreeItem<Node>> children = root.getChildren();
-            for(StackNode node : ne.nodes)
-            {
-                children.add(0,buildTree(node));
+            for (StackNode node : ne.nodes) {
+                children.add(0, createNode(node));
             }
             _TreeView.setValue(root);
-        })  ;
+        });
     }
 
-    private TreeItem<Node> buildTree(Node n)
-    {
-        TreeItem<Node> root = new TreeItem<>(n);
-        root.setExpanded(true);
-        java.util.List<Node> references = n.getReferences();
-        ObservableList<TreeItem<Node>> children = root.getChildren();
-        for(Node child : references)
-        {
-            children.add(buildTree(child));
-        }
+    /**
+     * Creates nodes on the fly for the treeView
+     * Avoids stack overflow / infinite recursion by lazy loading
+     * cycles can are represented like in a normal debugger
+     *
+     * @param n
+     * @return
+     */
+    private TreeItem<Node> createNode(final Node n) {
+        return new TreeItem<Node>(n) {
+            private boolean isLeaf;
+            private boolean isFirstTimeChildren = true;
+            private boolean isFirstTimeLeaf = true;
 
-        return root;
+            @Override
+            public ObservableList<TreeItem<Node>> getChildren() {
+                if (isFirstTimeChildren) {
+                    isFirstTimeChildren = false;
+                    super.getChildren().setAll(buildChildren(this));
+                }
+                return super.getChildren();
+            }
+
+            @Override
+            public boolean isLeaf() {
+                if (isFirstTimeLeaf) {
+                    isFirstTimeLeaf = false;
+                    Node node = getValue();
+                    isLeaf = node.getReferences().isEmpty();
+                }
+
+                return isLeaf;
+            }
+
+            private ObservableList<TreeItem<Node>> buildChildren(TreeItem<Node> TreeItem) {
+                Node node = TreeItem.getValue();
+                if (node != null) {
+                    List<Node> references = node.getReferences();
+                    if (references != null && !references.isEmpty()) {
+                        ObservableList<TreeItem<Node>> children = FXCollections.observableArrayList();
+
+                        for (Node child : references) {
+                            children.add(createNode(child));
+                        }
+
+                        return children;
+                    }
+                }
+
+                return FXCollections.emptyObservableList();
+            }
+        };
     }
 
 
